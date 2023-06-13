ea1ecefbc460bf763b22450e30aa2f4f09ca53b7
min33sky <min33sky@naver.com>
2023-06-13 23:42:14 +0900

    feat: add AppIcon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { motion, useMotionValue } from 'framer-motion';
 import AppIcon from './components/AppIcon';
+import {
+  FaFacebook,
+  FaGoogle,
+  FaApple,
+  FaGithub,
+  FaMicrosoft,
+  FaAmazon,
+} from 'react-icons/fa';
+import { IconType } from 'react-icons';
 
 export default function App() {
   //? Infinity는 변환을 깨끗하게 유지시키기 위한 Null값으로 사용된다.
   //? 지정한 범위를 벗어나기 때문에 값을 리셋시킬 수 있다.
   const mouseX = useMotionValue(Infinity);
 
+  const icons: IconType[] = [
+    FaFacebook,
+    FaGoogle,
+    FaApple,
+    FaGithub,
+    FaMicrosoft,
+    FaAmazon,
+  ];
+
   return (
-    <div className="relative h-screen bg-slate-400">
+    <div className="relative h-screen bg-gradient-to-br from-slate-800 to-slate-600">
       <motion.div
         onMouseMove={(e) => {
           mouseX.set(e.pageX);
@@ -15,10 +33,10 @@ export default function App() {
         onMouseLeave={() => {
           mouseX.set(Infinity);
         }}
-        className="fixed bottom-14 left-1/2 mx-auto flex h-16 max-w-xl -translate-x-1/2 items-end gap-4 rounded-2xl bg-gray-700 px-4 pb-3"
+        className="fixed bottom-14 left-1/2 mx-auto flex h-16 max-w-xl -translate-x-1/2 items-end gap-4 rounded-2xl bg-white/10 px-4 pb-3"
       >
-        {[...Array(6).keys()].map((i) => (
-          <AppIcon key={i} mouseX={mouseX} />
+        {icons.map((icon, idx) => (
+          <AppIcon key={idx} mouseX={mouseX} icon={icon} />
         ))}
       </motion.div>
     </div>
--- src/components/AppIcon.tsx
+++ src/components/AppIcon.tsx
@@ -1,17 +1,22 @@
 import { MotionValue, motion, useSpring, useTransform } from 'framer-motion';
 import { useRef } from 'react';
+import { IconType } from 'react-icons';
 
 interface AppIconProps {
   mouseX: MotionValue;
+  icon: IconType;
 }
 
-export default function AppIcon({ mouseX }: AppIconProps): JSX.Element {
+export default function AppIcon({
+  mouseX,
+  icon: Icon,
+}: AppIconProps): JSX.Element {
   const ref = useRef<HTMLDivElement>(null);
 
+  //* 아이콘 중앙 좌표를 기준으로 거리를 계산
   const distance = useTransform(mouseX, (val) => {
     const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
-
-    return val - bounds.x - bounds.width / 2;
+    return val - bounds.x - bounds.width / 2; //? 0이면 아이콘 중앙을 의미
   });
 
   const widthSync = useTransform(distance, [-150, 0, 150], [40, 100, 40]);
@@ -25,7 +30,9 @@ export default function AppIcon({ mouseX }: AppIconProps): JSX.Element {
     <motion.div
       ref={ref}
       style={{ width }}
-      className="aspect-square w-10 rounded-full bg-gray-400"
-    />
+      className="aspect-square w-10 overflow-hidden rounded-full bg-gray-400 p-1"
+    >
+      <Icon className="h-full w-full text-gray-700" />
+    </motion.div>
   );
 }
